abce9ca3044137c4e1a4aa0fecab6052285b322f
tmak1000 <watanabe4919@mx36.tiki.ne.jp>
2014-06-21 23:44:09 +0000

    First add puzzle.cpp

---
+++ puzzle.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<cstdlib>
+using std::cin;
+using std::cout;
+
+int main(){
+  for(int row = 1;row < 8;row++){
+    for(int col = 0;col < 4 - abs(4-row);col++){
+      cout<<"#";
+    }
+  }
+  cout<<"\n";
+}
