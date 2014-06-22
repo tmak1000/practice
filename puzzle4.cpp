41c61509d48fd8db85e2d5d8040fb19ef6a910c1
tmak1000 <watanabe4919@mx36.tiki.ne.jp>
2014-06-22 01:45:55 +0000

    First add puzzle4

---
+++ puzzle4.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<cstdlib>
+using std::cout;
+using std::cin;
+
+int main(){
+    for(int row = 0;row<4;row++){     
+	for(int space=0;space <3-row;space++){
+	  cout<<" ";
+	}
+	for(int i = 0;i<2;i++){
+	for(int col = 0;col<4-abs(3-row);col++){
+            cout<<"#";
+	}
+	}
+            cout<<"\n";
+    }
+
+  }
+    
