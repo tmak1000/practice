2e9c73873a5d8bfb47405990209407e2b37ece74
tmak1000 <watanabe4919@mx36.tiki.ne.jp>
2014-06-22 02:11:22 +0000

    Add temp

--- puzzle4.cpp
+++ puzzle4.cpp
@@ -16,5 +16,21 @@ int main(){
             cout<<"\n";
     }
 
+    for(int row = 0;row<4;row++){
+      if(row>0){
+	for(int space=0;space <row;space++){
+	  cout<<" ";
+	}
+      }
+      for(int i = 0;i<2;i++){	
+	for(int col = 0;col<4-row;col++){
+	  cout<<"#";
+	}     
+
+      }
+      cout<<"\n";
+    }
+
+
   }
     
