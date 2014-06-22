a74d31fbbfa25057002081259643f8c025624284
tmak1000 <watanabe4919@mx36.tiki.ne.jp>
2014-06-22 01:30:31 +0000

    First add puzzle3

---
+++ puzzle3.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<cstdlib>
+using std::cout;
+using std::cin;
+
+int main(){
+    for(int row = 0;row<4;row++){
+      if(row > 0) { 
+	for(int space=0;space < row;space++){
+	  cout<<" ";
+	}
+      }
+      for(int col = 0;col<4-row;col++){
+      
+      cout<<"#";
+      }
+      for(int col = 0;col<4-row;col++){
+
+	cout<<"#";
+      }
+
+      cout<<"\n";
+    }
+
+  }
+    
