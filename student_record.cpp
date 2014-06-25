c23f11f1cd658b9a1b994514e993d54c6290141a
tmak1000 <watanabe4919@mx36.tiki.ne.jp>
2014-06-25 13:58:10 +0000

    first add

---
+++ student_record.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using std::cin;
+using std::cout;
+
+struct listNode {
+  int studentNum;
+  int grade;
+  listNode * next;
+};
+typedef listNode * studentCollection;
+
+int main(){
+
+
+};
+
